67cebe6d9816ab2d12739860c3141d40799a32ed
Igor Chevdar <igor.chevdar@jetbrains.com>
2018-04-25 17:27:42 +0300

    DFG: Supported IrFunctionReference

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/optimizations/Devirtualization.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/optimizations/Devirtualization.kt
@@ -361,6 +361,9 @@ internal object Devirtualization {
                         is DataFlowIR.Node.StaticCall ->
                             dfs(node.callee, node.returnType)
 
+                        is DataFlowIR.Node.FunctionReference ->
+                            dfs(node.symbol, node.returnType)
+
                         is DataFlowIR.Node.VirtualCall -> {
                             if (node.receiverType == DataFlowIR.Type.Virtual)
                                 continue@nodeLoop
@@ -913,6 +916,10 @@ internal object Devirtualization {
 
                         is DataFlowIR.Node.AllocInstance -> {
                             concreteClass(node.type.resolved())
+                        }
+
+                        is DataFlowIR.Node.FunctionReference -> {
+                            sourceNode(concreteType(node.type.resolved())) { "Const\$${function.symbol}" }
                         }
 
                         is DataFlowIR.Node.FieldRead ->
